e9bb4171c5a75cc118c59b3b8f4f48ad61bee9fe
Vivek <vivekhegde998@gmail.com>
2020-08-13 19:48:59 +0530

    Updates

---
+++ precache-manifest.812797f3f3817dfe35d8aee8218e870b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11f8dfb0d1c97b0ce10ebf113eb7cb99",
+    "url": "/Bewgle-Assessment/index.html"
+  },
+  {
+    "revision": "f6ff53a08d0ca625da84",
+    "url": "/Bewgle-Assessment/static/css/main.44154a4a.chunk.css"
+  },
+  {
+    "revision": "9859aaf149ffd4f299fc",
+    "url": "/Bewgle-Assessment/static/js/2.8c7cf624.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Bewgle-Assessment/static/js/2.8c7cf624.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6ff53a08d0ca625da84",
+    "url": "/Bewgle-Assessment/static/js/main.9ef55caa.chunk.js"
+  },
+  {
+    "revision": "6fecf08ec49d404d1f78",
+    "url": "/Bewgle-Assessment/static/js/runtime-main.8963d005.js"
+  },
+  {
+    "revision": "7cd92069f47b48e50dedd69f7a704558",
+    "url": "/Bewgle-Assessment/static/media/Illustration1.7cd92069.png"
+  },
+  {
+    "revision": "dfb2ea3c2ab3a8dafafe2226ac6fb008",
+    "url": "/Bewgle-Assessment/static/media/Illustration2.dfb2ea3c.png"
+  },
+  {
+    "revision": "df92c81182058492697117d089016820",
+    "url": "/Bewgle-Assessment/static/media/Illustration3.df92c811.png"
+  }
+]);
